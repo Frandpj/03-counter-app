cdb5648bc03160aa5b2f3d1fbe65215f2727697d
Frandpj <fdelpinojimenez@gmail.com>
2021-02-27 19:59:42 +0100

    Pruebas al archivo 08

--- src/tests/base/07-deses-arr.test.js
+++ src/tests/base/07-deses-arr.test.js
@@ -2,7 +2,7 @@ import { retornaArreglo } from '../../base/07-deses-arr';
 
 describe('Pruebas en desestructuración', () => {
     
-    test('debe de retornar un string y un número', () => {
+    test('Debe de retornar un string y un número', () => {
         
         const [letras, numeros] = retornaArreglo(); // ['ABC', 123]
 
---
+++ src/tests/base/08-imp-exp.test.js
@@ -0,0 +1,53 @@
+import { getHeroeById, getHeroesByOwner } from "../../base/08-imp-exp";
+import heroes from "../../data/heroes";
+
+describe('Pruebas en funciones de Héroes', () => {
+    
+    test('Debe de retornar un héroe por id', () => {
+        
+        const id = 1;
+        const heroe = getHeroeById(id);
+
+        const heroeData = heroes.find(heroe => heroe.id === id)
+
+        expect(heroe).toEqual(heroeData);
+
+    })
+
+    test('Debe de retornar undefined si el héroe no existe', () => {
+        
+        const id = 10;
+        const heroe = getHeroeById(id);
+
+        expect(heroe).toBe(undefined);
+
+    })
+
+    // Tarea
+    // 1 - Debe de retornar un arreglo con los héroes de DC
+    // Cambiar la variable id a por owner
+    // Usar toEqual al arreglo filtrado
+    test('Debe de retornar un arreglo con los héroes de DC', () => {
+        
+        const owner = 'DC';
+        const heroes = getHeroesByOwner(owner);
+        const heroesData = heroes.filter((h) => h.owner === owner );
+
+        expect(heroes).toEqual(heroesData);
+
+    })
+
+    // 2 -  Debe de retornar un arreglo con los héroes de Marvel
+    // length = 2
+    // Usar toBe
+    test('Debe de retornar un la longitud del arreglo con los héroes de Marvel', () => {
+        
+        const owner = 'Marvel';
+        const heroes = getHeroesByOwner(owner);
+
+        expect(heroes.length).toBe(2);
+
+    })
+    
+
+})
