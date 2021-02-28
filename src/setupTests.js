27f351e37b03612bf5adb26167fd496461025340
Frandpj <fdelpinojimenez@gmail.com>
2021-02-28 18:15:47 +0100

    Revisar elementos dentro del componente

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,9 @@
-import '@testing-library/jest-dom/extend-expect';
+// import '@testing-library/jest-dom/extend-expect';
+// Funciones de pruebas para crear snapshot
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+// Crea un snapshot más familiar
+import { createSerializer } from 'enzyme-to-json';
+
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,19 +1,52 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import '@testing-library/jest-dom'
+import { shallow } from 'enzyme';
+
 import PrimeraApp from "../PrimeraApp";
 
 describe('Pruebas en <PrimeraApp />', () => {
     
-    test('Debe de mostrar el mensaje "Hola soy Francisco"', () => {
+    // test('Debe de mostrar el mensaje "Hola soy Francisco"', () => {
    
-        const saludo = 'Hola soy Francisco';
+    //     const saludo = 'Hola soy Francisco';
 
-        // Desestructuración
-        const {getByText} = render(<PrimeraApp saludo = {saludo}/>)
+    //     // Desestructuración
+    //     const {getByText} = render(<PrimeraApp saludo = {saludo}/>)
 
-        expect(getByText(saludo)).toBeInTheDocument();
+    //     expect(getByText(saludo)).toBeInTheDocument();
+
+    // });
+
+    test('Debe de mostrar <PrimeraAopp /> correctamente', () => {
+        
+        const saludo = "Hola soy Francisco";
+        const wrapper = shallow(<PrimeraApp saludo = {saludo}/>);
+
+        // Crea un snapshot de lo que enviamos, en este caso el componente completo
+        // <PrimeraApp />
+        expect(wrapper).toMatchSnapshot();
 
     });
+
+    test('Debe de mostrar el subtitulo enviado por props', () => {
+
+        const saludo = "Hola soy Francisco";
+        const subtitulo = "Soy un subtitulo";
+
+        const wrapper = shallow(
+            <PrimeraApp 
+            saludo = {saludo}
+            subtitulo = {subtitulo}
+            />
+        );
+       
+        const textoParrafo = wrapper.find('p').text();
+        
+        expect(textoParrafo).toBe(subtitulo);
+        
+    });
+    
+    
     
 
 });
