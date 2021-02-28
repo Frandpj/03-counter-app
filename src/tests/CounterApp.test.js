efe7af419f79de25518257bd8e66a10a926604af
Frandpj <fdelpinojimenez@gmail.com>
2021-02-28 19:16:19 +0100

    Simular eventos - Click

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -5,11 +5,19 @@ import { shallow } from 'enzyme';
 import CounterApp from "../CounterApp";
 
 describe('Pruebas de <CounterApp />', () => {
+
+    // Variable global para todo lo de el método describe por debajo
+    let wrapper = shallow(<CounterApp />); // No se pierden las ayudas
+    
+    // Función que se ejecuta antes de cada test
+    beforeEach(() => {
+
+        wrapper = shallow(<CounterApp />);
+
+    });
     
     test('Debe de mostrar <CounterApp /> correctamente', () => {
    
-        const wrapper = shallow(<CounterApp />);
-
         // Crea un snapshot de lo que enviamos, en este caso el componente completo
         // <CounterApp />
         expect(wrapper).toMatchSnapshot();
@@ -24,7 +32,30 @@ describe('Pruebas de <CounterApp />', () => {
 
         expect(counterText).toBe('100');
 
-    })
+    });
+
+    test('Debe de incrementar con el botón +1', () => {
+       
+        // 'at' Busca los botones y coge el primero que encuentre
+        // 'simulate' Simula un click en el botón
+        const btn1 = wrapper.find('button').at(0).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        expect(counterText).toBe('11');
+        // INFORMACIÓN Muestra el html del botón
+        // console.log(btn1.html());
+
+    });
+
+    test('Debe de decrementar con el botón -1', () => {
+       
+        // 'at' Busca los botones y coge el primero que encuentre
+        // 'simulate' Simula un click en el botón
+        const btn2 = wrapper.find('button').at(1).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        expect(counterText).toBe('9');
+
+    });
+    
     
     
 
